a4527679006043d62b41fc9d4358d0589673849a
Franck-lab <franckfzr@oultook.com>
2025-04-17 01:30:15 -0800

    Main home menu done

--- TimeTracker.cpp
+++ TimeTracker.cpp
@@ -8,14 +8,20 @@ using namespace std;
 
 void displayBanner();
 void displayLogin(char[], int, int&);
+void displayMenu(char []);
 
 int main()
 {
     char name[20];
     int ID;
+    int choice;
     displayBanner();
     displayLogin(name, 20, ID);
-
+    // Login fixture, verification or authorization
+    displayMenu(name);
+    
+    cout << "Enter your choice (1-6): ";
+    cin >> choice;
 
     return 0;
 }
@@ -32,4 +38,15 @@ void displayLogin(char name[], int size, int& ID) {
     cin.getline(name, 20);
     cout << "Employee ID: ";
     cin >> ID;
+}
+
+void displayMenu(char name[]) {
+    cout << "\n------------ Home -------------\n\n";
+    cout << name << "\tClock in: Yes\t" << "Worked Hours: " << 8.25 << endl; // Change here with time stamp and calculated hours
+    cout << "\n1. Punch In\n";
+    cout << "2. Punch Out\n";
+    cout << "3. Timetable\n";
+    cout << "4. Open Shift\n";
+    cout << "5. Take Time Off\n";
+    cout << "6. Exit\n\n";
 }
